80fb19e2871c0b0aaf8809e5a923a1ac8c178699
kpszeniczka <148539347+kpszeniczka@users.noreply.github.com>
2023-10-20 14:43:55 +0200

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int AA[10];
+    int n;
+    int sizeb = 0;
+    int pominieto = 0;
+    for (int i = 0; i < 10; i++) {
+        cin >> n;
+        AA[10] = n;
+        if (n < 0) sizeb++;
+    }
+    int* BB = new int(sizeb);
+    int idx = 0;
+    for (int i = 0; i < 10; i++) {
+        if (AA[i] < 0) {
+            BB[idx] = AA[i];
+            idx++;
+        }
+        else {
+            pominieto++;
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << BB[i] << endl;
+    }
+    cout << idx << pominieto << endl
+    
+
+    delete BB;
+    return 0;
+}

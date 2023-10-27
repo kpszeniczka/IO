b695206a8e010b20de51346c21e5eac20635f3f9
kpszeniczka <148539347+kpszeniczka@users.noreply.github.com>
2023-10-27 10:56:42 +0200

    Add files via upload

---
+++ zadanie 3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double kmToMiles(double km) {
+    return km * 0.621371;
+}
+
+double kmToNautMiles(double km) {
+    return km * 0.539957;
+}
+
+int main() {
+    double km;
+
+    cout << "Podaj odleglosc w kilometrach: ";
+    cin >> km;
+
+    double miles = kmToMiles(km);
+    double nautmiles = kmToNautMiles(km);
+
+    cout << km << " kilometrow to " << miles << " mil lądowych." << endl;
+    cout << km << " kilometrow to " << nautmiles << " mil morskich." << endl;
+
+    return 0;
+}

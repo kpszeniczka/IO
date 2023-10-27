c594936d17ade3b888b95b8c341b506a0cd7050c
kpszeniczka <148539347+kpszeniczka@users.noreply.github.com>
2023-10-27 14:44:50 +0200

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Faktura {
+	string daneklienta;
+	int nrfaktury;
+	float kwota;
+	bool zaplacona;
+public:
+	Faktura() {
+		daneklienta = "";
+		nrfaktury = 0;
+		kwota = 0;
+		zaplacona = false;
+	}
+	void dodaj(string d, int nr, float k, bool z) {
+		this->daneklienta = d;
+		this->nrfaktury = nr;
+		this->kwota = k;
+		this->zaplacona = z;
+	}
+	void oplac() {
+		zaplacona = true;
+	}
+	void zmiendane(string d, float k) {
+		this->daneklienta = d;
+		this->kwota = k;
+		zaplacona = false;
+	}
+	void wyswietl() {
+		cout << "Faktura nr: " << nrfaktury << endl << "Na nazwisko: " << daneklienta << endl << "Kwota: " << kwota << endl << "Czy opłacona: " << zaplacona;
+	}
+};
+
+
+int main() {
+	
+	Faktura f1;
+
+	f1.dodaj("Jan Kowalski", 291723, 1231.123, 1);
+
+	f1.wyswietl();
+
+	return 0;
+}

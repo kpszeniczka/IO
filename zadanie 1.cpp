19757a927b50145857687c8a67892770a1f7e6ae
kpszeniczka <148539347+kpszeniczka@users.noreply.github.com>
2023-10-27 11:03:22 +0200

    Add files via upload

---
+++ zadanie 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 301; i <= 1200; i++) {
+        if (i % 2 != 0) {
+            cout << i;
+            if (i % 100 == 0) {
+                cout << " *** ";
+            }
+            else if (i % 400 == 0) {
+                cout << endl;
+            }
+            else {
+                cout << " ";
+            }
+        }
+    }
+
+    return 0;
+}
